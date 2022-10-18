35c51ae30c872c9540fa8559e93c79434af4e4f1
Michael de Senna <desenna@gmail.com>
2022-10-18 05:17:22 -0400

    feat: add aliases.zsh
    Signed-off-by: Michael de Senna <desenna@gmail.com>

---
+++ zsh/aliases.zsh
@@ -0,0 +1,23 @@
+## Navigation
+alias ..='cd ..'
+alias ...='cd ../../'
+alias .3='cd ../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../../'
+
+## Fix typos
+alias cd..='cd ..'
+
+## Safe operations
+alias cl='clear'
+alias h='history'
+alias ll='ls -lah'
+
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+## Confirm for unsafe operations
+alias cp='cp -i'
+alias ln='ln -i'
+alias mv='mv -i'
